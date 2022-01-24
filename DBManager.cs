360787a3101d0ff94f7ff681d6a3a6bcd29964bd
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2022-01-24 15:11:23 -0600

    Update to use using statements

--- DBManager.cs
+++ DBManager.cs
@@ -7,6 +7,8 @@ namespace ExcelReader
 {
     class DBManager
     {
+        private static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
         public static void StartUpDatabaseOperations(string DatabaseName)
         {
             if (!DoesDatabaseExist(DatabaseName))
@@ -29,203 +31,225 @@ namespace ExcelReader
         }
         public static void CreateDatabase(string DatabaseName)
         {
-            string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
-            SqlConnection myConn = new SqlConnection(connectionString);
-
-            string str = $"CREATE DATABASE {DatabaseName}";
-            SqlCommand myCommand = new SqlCommand(str, myConn);
-
-            myConn.Open();
-            myCommand.ExecuteNonQuery();
-
-            myConn.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $"CREATE DATABASE {DatabaseName}";
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static void DeleteDatabase(string DatabaseName)
         {
-            string conString = ConfigurationManager.AppSettings.Get("ConnectionString");
-            var con = new SqlConnection(conString);
-            con.Open();
-            string dropDBString = $@"EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'{DatabaseName}'
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $@"EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'{DatabaseName}'
                                     USE [master]
                                     DROP DATABASE [{DatabaseName}]";
-            SqlCommand dropDBCommand = new SqlCommand(dropDBString, con);
-            dropDBCommand.ExecuteNonQuery();
-            con.Close();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static void AddTable(string tableName)
         {
-            SqlConnection con = OpenSql();
-            string command = $@"CREATE TABLE [dbo].[{tableName}](
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $@"CREATE TABLE [dbo].[{tableName}](
                                 [Id][int] IDENTITY(1, 1) NOT NULL)";
-
-            SqlCommand myCommand = new SqlCommand(command, con);
-            myCommand.ExecuteNonQuery();
-            con.Close();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static void AddColumn(string tableName, string columnName)
         {
-            SqlConnection con = OpenSql();
-            string command = $"ALTER TABLE [dbo].[{tableName}] ADD {columnName} VARCHAR(255)";
-
-            SqlCommand myCommand = new SqlCommand(command, con);
-            myCommand.ExecuteNonQuery();
-            con.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $"ALTER TABLE [dbo].[{tableName}] ADD {columnName} VARCHAR(255)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static void UpdateColumns(string tableName, string columnName, string data, int index)
         {
-            SqlConnection con = OpenSql();
-
-            string command = $@"UPDATE {tableName}
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $@"UPDATE {tableName}
                              SET {columnName} = '{data}'
                              WHERE Id = {index}";
-
-            SqlCommand myCommand = new SqlCommand(command, con);
-            myCommand.ExecuteNonQuery();
-            con.Close();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static void InsertToColumn(string tableName, string columnName, string data)
         {
-            SqlConnection con = OpenSql();
-
-            string command = $"INSERT INTO {tableName}({columnName}) VALUES ('{data}')";
-
-            SqlCommand myCommand = new SqlCommand(command, con);
-            myCommand.ExecuteNonQuery();
-            con.Close();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $"INSERT INTO {tableName}({columnName}) VALUES ('{data}')";
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public static bool DoesDatabaseExist(string DatabaseName)
         {
-            string cmdText = $"SELECT * FROM master.dbo.sysdatabases WHERE name = '{DatabaseName}'";
             bool isExist = false;
-            string conString = ConfigurationManager.AppSettings.Get("ConnectionString");
-            var con = new SqlConnection(conString);
-            con.Open();
-            using (SqlCommand cmd = new SqlCommand(cmdText, con))
+            using (var connection = new SqlConnection(connectionString))
             {
-                using (SqlDataReader reader = cmd.ExecuteReader())
+                using (var cmd = connection.CreateCommand())
                 {
-                    isExist = reader.HasRows;
+                    connection.Open();
+                    cmd.CommandText = $"SELECT * FROM master.dbo.sysdatabases WHERE name = '{DatabaseName}'";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        isExist = reader.HasRows;
+                    }
                 }
             }
-            con.Close();
 
             return isExist;
         }
         public static bool DoesTableExist(string tableName)
         {
             bool isExist = false;
-            SqlConnection con = OpenSql();
-            string command = $@"SELECT *
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $@"SELECT *
                                 FROM INFORMATION_SCHEMA.TABLES
                                 WHERE TABLE_NAME = N'{tableName}'";
-            using (SqlCommand cmd = new SqlCommand(command, con))
-            {
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    isExist = reader.HasRows;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        isExist = reader.HasRows;
+                    }
                 }
             }
-            con.Close();
             return isExist;
         }
         public static bool DoesColumnExist(string tableName, string columnName)
         {
             bool isExist = false;
-            SqlConnection con = OpenSql();
-            string command = $@"SELECT * 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = $@"SELECT * 
                                 FROM INFORMATION_SCHEMA.COLUMNS 
                                 WHERE table_name = '{tableName}'
                                 AND column_name = '{columnName}'";
-            using (SqlCommand cmd = new SqlCommand(command, con))
-            {
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    isExist = reader.HasRows;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        isExist = reader.HasRows;
+                    }
                 }
             }
-            con.Close();
             return isExist;
+
         }
         public static List<List<object>> ReadAllRowsFromTable(string tableName)
         {
             List<List<object>> allColumnsData = new List<List<object>>();
             int iteration = 0;
 
-            string command = $"SELECT * FROM {tableName}";
-
-            SqlConnection con = OpenSql();
-            SqlCommand myCommand = new SqlCommand(command, con);
-            SqlDataReader dataReader = myCommand.ExecuteReader();
-            
-            while (dataReader.Read())
+            using (var connection = new SqlConnection(connectionString))
             {
-                int numberOfFields = dataReader.FieldCount;
-
-                List<object> currentColumnData = new List<object>(numberOfFields);
-                for(int i = 0; i < numberOfFields; i++)
+                using (var command = connection.CreateCommand())
                 {
-                    currentColumnData.Add(dataReader[i].ToString());
+                    connection.Open();
+                    command.CommandText = $"SELECT * FROM {tableName}";
+                    using (var dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            int numberOfFields = dataReader.FieldCount;
+
+                            List<object> currentColumnData = new List<object>(numberOfFields);
+                            for (int i = 0; i < numberOfFields; i++)
+                            {
+                                currentColumnData.Add(dataReader[i].ToString());
+                            }
+                            allColumnsData.Add(currentColumnData);
+                            iteration++;
+                        }
+                    }
                 }
-                allColumnsData.Add(currentColumnData);
-                iteration++;
             }
-            con.Close();
             
             return allColumnsData;
         }
         public static List<string> GetAllColumnHeadingsFromTable(string tableName)
         {
             List<string> allHeadings = new List<string>();
-            string command = $@"SELECT COLUMN_NAME
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = $@"SELECT COLUMN_NAME
                                FROM INFORMATION_SCHEMA.COLUMNS
                                 WHERE TABLE_NAME = N'{tableName}'";
-
-            SqlConnection con = OpenSql();
-            SqlCommand myCommand = new SqlCommand(command, con);
-            SqlDataReader dataReader = myCommand.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                string currentHeading = dataReader[0].ToString();
-                if(currentHeading == "Id")
-                {
-                    currentHeading = "";
+                    using (var dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            string currentHeading = dataReader[0].ToString();
+                            if (currentHeading == "Id")
+                            {
+                                currentHeading = "";
+                            }
+                            allHeadings.Add(currentHeading);
+                        }
+                    }
                 }
-                allHeadings.Add(currentHeading);
             }
-            con.Close();
-
             return allHeadings;
         }
         public static List<string> GetAllTablesFromDB()
         {
             List<string> allTables = new List<string>();
-            string command = $@"SELECT TABLE_NAME
-                               FROM INFORMATION_SCHEMA.TABLES";
 
-            SqlConnection con = OpenSql();
-            SqlCommand myCommand = new SqlCommand(command, con);
-            SqlDataReader dataReader = myCommand.ExecuteReader();
-
-            while (dataReader.Read())
+            using (var connection = new SqlConnection(connectionString))
             {
-                string currentHeading = dataReader[0].ToString();
-                if (currentHeading == "Id")
+                using (var command = connection.CreateCommand())
                 {
-                    currentHeading = "";
+                    connection.Open();
+                    command.CommandText = $@"SELECT TABLE_NAME
+                               FROM INFORMATION_SCHEMA.TABLES";
+                    using (var dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            string currentHeading = dataReader[0].ToString();
+                            if (currentHeading == "Id")
+                            {
+                                currentHeading = "";
+                            }
+                            allTables.Add(currentHeading);
+                        }
+                    }
                 }
-                allTables.Add(currentHeading);
             }
-            con.Close();
 
             return allTables;
-        }
-        public static SqlConnection OpenSql()
-        {
-            string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") + $"Initial Catalog={ConfigurationManager.AppSettings.Get("DatabaseName")}";
-            var connection = new SqlConnection(connectionString);
-            connection.Open();
-            return connection;
         }
     }
 }
